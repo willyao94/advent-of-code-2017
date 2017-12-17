5187728d44b95ffc19aebbc276af219d5acb27b1
Will <williamyao1994@gmail.com>
2017-12-17 10:22:48 -0800

    Need code reviews

--- day17.py
+++ day17.py
@@ -3,10 +3,9 @@ steps = int(open('./Inputs/day17.txt').read().strip())
 # Part 1
 buffer = [0]
 ptr = 0
-for n in xrange(1,2019):
-    ptr = (ptr + steps) % len(buffer)
-    buffer.insert(ptr+1, n)
-    ptr+=1
+for n in range(1,2019):
+    ptr = ((ptr + steps) % n) + 1
+    buffer.insert(ptr, n)
 ptr = buffer.index(2017)
 print("Part 1:", buffer[(ptr+1)%len(buffer)])
 
@@ -15,7 +14,7 @@ ptr = 0
 # Value 0 always stays at index 0
 # Keep track of the values in index 1
 cache = []
-for n in xrange(1,50000001):
+for n in range(1,50000001):
     ptr = ((ptr + steps) % n) + 1
     if ptr == 1:
         cache.append(n)
