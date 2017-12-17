91bd2184108a5fe2be41bd9b549c9de624493e73
Will <williamyao1994@gmail.com>
2017-12-16 22:36:13 -0800

    Day 17

---
+++ day17.py
@@ -0,0 +1,22 @@
+steps = int(open('./Inputs/day17.txt').read().strip())
+
+# Part 1
+buffer = [0]
+ptr = 0
+for n in xrange(1,2019):
+    ptr = (ptr + steps) % len(buffer)
+    buffer.insert(ptr+1, n)
+    ptr+=1
+ptr = buffer.index(2017)
+print("Part 1:", buffer[(ptr+1)%len(buffer)])
+
+# Part 2
+ptr = 0
+# Value 0 always stays at index 0
+# Keep track of the values in index 1
+cache = []
+for n in xrange(1,50000001):
+    ptr = ((ptr + steps) % n) + 1
+    if ptr == 1:
+        cache.append(n)
+print("Part 2:", cache[-1])
